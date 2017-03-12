abc813dd72fb842215b396557f3307e42e3fa325
Aaron Murphy <aamurphy@deloitte.com>
2017-03-11 21:33:34 -0500

    formatting

--- ContextMenu.js
+++ ContextMenu.js
@@ -2,62 +2,66 @@ import React from 'react';
 import './style.css';
 
 export default class ContextMenu extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            target: ''
-        }
+    this.state = {
+      target: ''
     }
+  }
 
-    componentDidMount () {
-        let context = document.getElementById(this.props.contextID);
-        context.addEventListener('contextmenu', () => {this.openContextMenu(event)});
+  componentDidMount() {
+    let context = document.getElementById(this.props.contextID);
+    context.addEventListener('contextmenu', () => {
+      this.openContextMenu(event)
+    });
 
-        let menu = document.getElementById('contextMenu');
-        menu.addEventListener('mouseleave', () => {this.closeContextMenu()});
+    let menu = document.getElementById('contextMenu');
+    menu.addEventListener('mouseleave', () => {
+      this.closeContextMenu()
+    });
 
-    }
+  }
 
-    render () {
-        return (
-            <div id="contextMenu">
-                {this.props.items.map((item) => {
-                        let clickHandler = () => {
-                            this.closeContextMenu();
-                            item.function(this.state.target);
-                        }
-                        let label = item.label;
-                        let icon = item.icon;
-                        return (
-                            <span onClick={clickHandler} key={label}>
-                                <img className="icon" src={icon} role="presentation" />
-                                {label}
-                            </span>
-                        );
-                    })}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div id="contextMenu">
+        {this.props.items.map((item) => {
+          let clickHandler = () => {
+            this.closeContextMenu();
+            item.function(this.state.target);
+          };
+          let label = item.label;
+          let icon = item.icon;
+          return (
+            <span onClick={clickHandler} key={label}>
+              <img className="icon" src={icon} role="presentation"/>
+              {label}
+            </span>
+          );
+        })}
+      </div>
+    );
+  }
 
-    openContextMenu = (event) => {
-        event.preventDefault();
-        this.setState({target: event.target});
+  openContextMenu = (event) => {
+    event.preventDefault();
+    this.setState({target: event.target});
 
-        let xOffset = Math.max(document.documentElement.scrollLeft, document.body.scrollLeft);
-        let yOffset = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+    let xOffset = Math.max(document.documentElement.scrollLeft, document.body.scrollLeft);
+    let yOffset = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
 
-        let menu = document.getElementById('contextMenu');
+    let menu = document.getElementById('contextMenu');
 
-        menu.style.cssText =
-            'left: ' + (event.clientX + xOffset) + 'px;' +
-            'top: ' + (event.clientY + yOffset) + 'px;' +
-            'visibility: visible;';
-    }
+    menu.style.cssText =
+      'left: ' + (event.clientX + xOffset) + 'px;' +
+      'top: ' + (event.clientY + yOffset) + 'px;' +
+      'visibility: visible;';
+  }
 
-    closeContextMenu = () => {
-        let menu = document.getElementById('contextMenu');
-        menu.style.cssText = 'visibility: hidden;';
+  closeContextMenu = () => {
+    let menu = document.getElementById('contextMenu');
+    menu.style.cssText = 'visibility: hidden;';
 
-    }
+  }
 }
