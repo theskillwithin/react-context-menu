3fb5e71a9eb4252960893f18b45cafcb57218773
Aaron Murphy <aamurphy@deloitte.com>
2017-03-11 21:46:09 -0500

    updating gitignore and adding latest build

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
@@ -21,78 +21,78 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var ContextMenu = function (_React$Component) {
-    _inherits(ContextMenu, _React$Component);
+  _inherits(ContextMenu, _React$Component);
 
-    function ContextMenu(props) {
-        _classCallCheck(this, ContextMenu);
+  function ContextMenu(props) {
+    _classCallCheck(this, ContextMenu);
 
-        var _this = _possibleConstructorReturn(this, (ContextMenu.__proto__ || Object.getPrototypeOf(ContextMenu)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (ContextMenu.__proto__ || Object.getPrototypeOf(ContextMenu)).call(this, props));
 
-        _this.openContextMenu = function (event) {
-            event.preventDefault();
-            _this.setState({ target: event.target });
+    _this.openContextMenu = function (event) {
+      event.preventDefault();
+      _this.setState({ target: event.target });
 
-            var xOffset = Math.max(document.documentElement.scrollLeft, document.body.scrollLeft);
-            var yOffset = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
+      var xOffset = Math.max(document.documentElement.scrollLeft, document.body.scrollLeft);
+      var yOffset = Math.max(document.documentElement.scrollTop, document.body.scrollTop);
 
-            var menu = document.getElementById('contextMenu');
+      var menu = document.getElementById('contextMenu');
 
-            menu.style.cssText = 'left: ' + (event.clientX + xOffset) + 'px;' + 'top: ' + (event.clientY + yOffset) + 'px;' + 'visibility: visible;';
-        };
+      menu.style.cssText = 'left: ' + (event.clientX + xOffset) + 'px;' + 'top: ' + (event.clientY + yOffset) + 'px;' + 'visibility: visible;';
+    };
 
-        _this.closeContextMenu = function () {
-            var menu = document.getElementById('contextMenu');
-            menu.style.cssText = 'visibility: hidden;';
-        };
+    _this.closeContextMenu = function () {
+      var menu = document.getElementById('contextMenu');
+      menu.style.cssText = 'visibility: hidden;';
+    };
 
-        _this.state = {
-            target: ''
-        };
-        return _this;
+    _this.state = {
+      target: ''
+    };
+    return _this;
+  }
+
+  _createClass(ContextMenu, [{
+    key: 'componentDidMount',
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      var context = document.getElementById(this.props.contextID);
+      context.addEventListener('contextmenu', function () {
+        _this2.openContextMenu(event);
+      });
+
+      var menu = document.getElementById('contextMenu');
+      menu.addEventListener('mouseleave', function () {
+        _this2.closeContextMenu();
+      });
     }
+  }, {
+    key: 'render',
+    value: function render() {
+      var _this3 = this;
 
-    _createClass(ContextMenu, [{
-        key: 'componentDidMount',
-        value: function componentDidMount() {
-            var _this2 = this;
+      return _react2.default.createElement(
+        'div',
+        { id: 'contextMenu' },
+        this.props.items.map(function (item) {
+          var clickHandler = function clickHandler() {
+            _this3.closeContextMenu();
+            item.function(_this3.state.target);
+          };
+          var label = item.label;
+          var icon = item.icon;
+          return _react2.default.createElement(
+            'span',
+            { onClick: clickHandler, key: label },
+            icon && _react2.default.createElement('img', { className: 'icon', src: icon, role: 'presentation' }),
+            label
+          );
+        })
+      );
+    }
+  }]);
 
-            var context = document.getElementById(this.props.contextID);
-            context.addEventListener('contextmenu', function () {
-                _this2.openContextMenu(event);
-            });
-
-            var menu = document.getElementById('contextMenu');
-            menu.addEventListener('mouseleave', function () {
-                _this2.closeContextMenu();
-            });
-        }
-    }, {
-        key: 'render',
-        value: function render() {
-            var _this3 = this;
-
-            return _react2.default.createElement(
-                'div',
-                { id: 'contextMenu' },
-                this.props.items.map(function (item) {
-                    var clickHandler = function clickHandler() {
-                        _this3.closeContextMenu();
-                        item.function(_this3.state.target);
-                    };
-                    var label = item.label;
-                    var icon = item.icon;
-                    return _react2.default.createElement(
-                        'span',
-                        { onClick: clickHandler, key: label },
-                        _react2.default.createElement('img', { className: 'icon', src: icon, role: 'presentation' }),
-                        label
-                    );
-                })
-            );
-        }
-    }]);
-
-    return ContextMenu;
+  return ContextMenu;
 }(_react2.default.Component);
 
 exports.default = ContextMenu;
